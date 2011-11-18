aa676b649ef7a06e8620f68604d9c560ee9d906a
Guillermo Pascual <akira.space@gmail.com>
2011-11-18 09:46:33 +0100

    Added url mp3

--- eggbit_data.js
+++ eggbit_data.js
@@ -5,45 +5,189 @@ exports.resources = {
         name: "Super Mario Bros 3",
         year: "1988",
         platform: "NES",
-        cover: "3",
-        music: "3",
+        cover: "http://images.nintendolife.com/games/nes/super_mario_bros_3/cover_large.jpg",
+        music: "http://eggbit.herokuapp.com/assets/sounds/Super%20Mario%20Bros.%203%20Music.mp3",
         level: "1"
     },
     {
         id: "2",
-        name: "Zelda",
-        year: "2",
-        platform: "2",
-        cover: "2",
-        music: "2",
-        level: "2"
+        name: "Zelda a think to the path",
+        year: "1991",
+        platform: "SNES",
+        cover: "http://www.emuparadise.me/fup/up/34385-Legend_of_Zelda,_The_-_A_Link_to_the_Past_(USA)-32.jpg",
+        music: "http://eggbit.herokuapp.com/assets/sounds/Zelda%20a%20think%20to%20the%20path.mp3",
+        level: "1"
     },
     {
         id: "3",
-        name: "2",
-        year: "2",
-        platform: "2",
-        cover: "2",
-        music: "2",
-        level: "2"
+        name: "Street Fighter 2",
+        year: "1991",
+        platform: "Super Nintendo",
+        cover: "http://garoo9.com/elevator-kart/EK_files/psx/sfii_movie/cover%201.jpg",
+        music: "http://eggbit.herokuapp.com/assets/sounds/Street_Fighter2.mp3",
+        level: "1"
     },
     {
         id: "4",
-        name: "2",
-        year: "2",
-        platform: "2",
-        cover: "2",
-        music: "2",
-        level: "2"
+        name: "Donkey Kong Country 1",
+        year: "1994",
+        platform: "SNES",
+        cover: "http://centraldeletras.com/caratulas%20juegos/donkey%20kong%20country%201%201.jpg",
+        music: "http://eggbit.herokuapp.com/assets/sounds/Donkey_Kong_Country_1.mp3",
+        level: "1"
     },
     {
         id: "5",
-        name: "2",
+        name: "Metal Gears Solid 1",
         year: "2",
-        platform: "2",
-        cover: "2",
-        music: "2",
-        level: "2"
+        platform: "1998",
+        cover: "http://www.losprimeros10.es/wp-content/uploads/2011/05/metal_gear_solid_pal.jpg",
+        music: "http://eggbit.herokuapp.com/assets/sounds/Metal%20Gears.mp3",
+        level: "1"
+    },
+     {
+        id: "6",
+        name: "Crash Bandicoot Intro",
+        year: "1996",
+        platform: "Playstation",
+        cover: "http://www.camelotgames.net/images/BadCharsCrash_Bandicoot.png",
+        music: "http://eggbit.herokuapp.com/assets/sounds/Crash%20Bandicoot%20Intro.mp3",
+        level: "1"
+    },
+    {
+        id: "7",
+        name: "Sonic",
+        year: "1991",
+        platform: "SNES",
+        cover: "1",
+        music: "http://eggbit.herokuapp.com/assets/sounds/Sonic.mp3",
+        level: "1"
+    },
+    {
+        id: "8",
+        name: "",
+        year: "",
+        platform: "SNES",
+        cover: "1",
+        music: "",
+        level: "1"
+    },
+    {
+        id: "9",
+        name: "",
+        year: "",
+        platform: "SNES",
+        cover: "1",
+        music: "",
+        level: "1"
+    },
+    {
+        id: "10",
+        name: "",
+        year: "",
+        platform: "SNES",
+        cover: "1",
+        music: "",
+        level: "1"
+    },
+
+    {
+        id: "11",
+        name: "",
+        year: "",
+        platform: "SNES",
+        cover: "1",
+        music: "",
+        level: "1"
+    },
+    {
+        id: "12",
+        name: "",
+        year: "",
+        platform: "SNES",
+        cover: "1",
+        music: "",
+        level: "1"
+    },
+    {
+        id: "13",
+        name: "",
+        year: "",
+        platform: "SNES",
+        cover: "1",
+        music: "",
+        level: "1"
+    },
+    {
+        id: "14",
+        name: "",
+        year: "",
+        platform: "SNES",
+        cover: "1",
+        music: "",
+        level: "1"
+    },
+    {
+        id: "15",
+        name: "",
+        year: "",
+        platform: "SNES",
+        cover: "1",
+        music: "",
+        level: "1"
+    },
+    {
+        id: "16",
+        name: "",
+        year: "",
+        platform: "SNES",
+        cover: "1",
+        music: "",
+        level: "1"
+    },
+    {
+        id: "17",
+        name: "",
+        year: "",
+        platform: "SNES",
+        cover: "1",
+        music: "",
+        level: "1"
+    },
+    {
+        id: "18",
+        name: "",
+        year: "",
+        platform: "SNES",
+        cover: "1",
+        music: "",
+        level: "1"
+    },
+    {
+        id: "19",
+        name: "",
+        year: "",
+        platform: "SNES",
+        cover: "1",
+        music: "",
+        level: "1"
+    },
+    {
+        id: "20",
+        name: "",
+        year: "",
+        platform: "SNES",
+        cover: "1",
+        music: "",
+        level: "1"
+    },{
+        id: "21",
+        name: "",
+        year: "",
+        platform: "SNES",
+        cover: "1",
+        music: "",
+        level: "1"
     }
     ]
 };
