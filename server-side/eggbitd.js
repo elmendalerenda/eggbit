af84f7dd7cb817741be6c970c5857260f82999f4
elmendalerenda <elmendalerenda@gmail.com>
2011-11-18 05:00:51 +0100

    service for new individual game

--- server-side/eggbitd.js
+++ server-side/eggbitd.js
@@ -16,6 +16,32 @@ eggbitd.get('/tracks/all', function(req, res) {
   res.send(JSON.stringify(resources), 200);
 });
 
+eggbitd.get('/game/new', function(req, res) {
+  
+   var game = [];
+   var questions = [];
+      
+   for(var i =0; i < 20; i += 1){
+    var id = randomInAnArray(questions)
+    questions.push(id);
+        
+        var answers = [id];
+        answers.push(randomInAnArray(answers));
+        answers.push(randomInAnArray(answers));
+        
+        answers.sort(function(){
+          return Math.random() > 0.5
+        });
+
+        var question = {};
+        question[id] = answers;
+        
+        game.push(question);
+      }
+  
+  res.send(JSON.stringify(game), 200);
+});
+
 eggbitd.get('/tracks/current-version', function(req, res) {
   res.send(JSON.stringify({version: '1.0'}), 200);
 });
@@ -27,6 +53,18 @@ var port = process.env.PORT || 3000;
 eggbitd.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+var randomInAnArray = function(list){
+  
+       var numQuestions = 40;
+    
+    var number = Math.floor(Math.random() * numQuestions); 
+    
+    while(list.indexOf(number) > -1 ){
+        number = Math.floor(Math.random() * numQuestions);
+    }
+    return number;
+};
 
 
 
